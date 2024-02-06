3f46adfc8d6a833dd1cfdecee9bc99613579a2cb
craftwill <william-gin1@hotmail.com>
2024-02-06 17:58:33 -0500

    Fix some errors and unused code

--- Assets/Scripts/Game/MultiplayerGameManager.cs
+++ Assets/Scripts/Game/MultiplayerGameManager.cs
@@ -13,17 +13,12 @@ namespace Kraken
     [DisallowMultipleComponent]
     public class MultiplayerGameManager : MonoBehaviourPunCallbacks, ILobbyCallbacks
     {
-        int playerCount = 0;
-
         [SerializeField] private Transform _playersRespawnPos;
         [SerializeField] private Transform _playersSpawnPos;
         [SerializeField] private Transform _respawnBoxTeleportPos;
         [SerializeField] private GameObject _playerPrefab;
 
         private GameObject _localPlayer;
-        private List<PlayerEntity> playerEntities;
-
-        private bool _gameStarted;
 
         public override void OnEnable()
         {
@@ -50,12 +45,6 @@ namespace Kraken
         public override void OnPlayerLeftRoom(Player otherPlayer)
         {
             base.OnPlayerLeftRoom(otherPlayer);
-
-            // Remove Player Entity that matches the UserId
-            playerEntities.RemoveAll((playerEntity) =>
-            {
-                return playerEntity.photonView.Owner.UserId == otherPlayer.UserId;
-            });
         }
 
         private void CreatePlayer()
@@ -65,24 +54,9 @@ namespace Kraken
             _localPlayer = NetworkUtils.Instantiate(_playerPrefab.name, _playersSpawnPos.position);
         }
 
-        private void TeleportPlayer(string playerUserId, Vector3 position)
-        {
-            var playerToTeleport = GetPlayerEntity(playerUserId);
-
-            // Sanity check
-            if (playerToTeleport == null) { return; }
-
-            TeleportPlayer(playerToTeleport.photonView, position);
-        }
-
         private void TeleportPlayer(PhotonView playerToTeleport, Vector3 position) 
         {
             playerToTeleport.transform.position = position;
-        }
-
-        private PlayerEntity GetPlayerEntity(string playerUserId) 
-        {
-            return playerEntities.Find(playerEntity => playerEntity.photonView.Owner.UserId == playerUserId);
         }
     }
 }
