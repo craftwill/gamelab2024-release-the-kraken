7873e4109ae555b07d06655268ae5a2d876b3d3c
craftwill <william-gin1@hotmail.com>
2024-02-28 14:14:38 -0500

    Progress Entities Animation

--- Assets/Scripts/Game/Entities/EntityAnimationComponent.cs
+++ Assets/Scripts/Game/Entities/EntityAnimationComponent.cs
@@ -3,32 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Photon.Pun;
+using Bytes;
 
 namespace Kraken
 {
-    public class EntityAnimationComponent : MonoBehaviourPun
+    public class EntityAnimationComponent : MonoBehaviour
     {
-        [SerializeField] private Animator _animator;
+        [SerializeField] private KrakenAnimationStateMachine _animStateMachine;
+        [SerializeField] private string _animatorPrefix;
         [SerializeField] private string _animatorSuffix;
 
         private int _hurtStep = 1;
 
         private void Start()
         {
-            if (_animator == null) return;
+            if (_animStateMachine.GetAnimator() == null) return;
 
-            _animator.speed = Random.Range(0.9f, 1.1f);
+            _animStateMachine.GetAnimator().speed = Random.Range(0.9f, 1.1f);
         }
 
         public void PlayHurtAnim()
         {
-            if (_animator == null) return;
+            if (_animStateMachine.GetAnimator() == null) return;
 
-            _animator.Play(_animatorSuffix + "_hurt" + _hurtStep);
+            _animStateMachine.GetAnimator().Play(_animatorSuffix + "_hurt" + _hurtStep);
             _hurtStep++;
             if (_hurtStep > 2)
             {
                 _hurtStep = 1;
+            }
+        }
+
+        // Animations used by every entity
+        public class EntityAnimState : KrakenBaseAnimState
+        {
+            static public readonly EntityAnimState Idle = new EntityAnimState("Idle", 1f);
+            static public readonly EntityAnimState Walk = new EntityAnimState("Walk", 1f);
+            static public readonly EntityAnimState Die = new EntityAnimState("Die", 1f);
+
+            public EntityAnimState(string pClipName, float pAnimSpeed = 1f, int pNbVariations = -1)
+                : base(pClipName, pAnimSpeed, pNbVariations)
+            {
+
             }
         }
     }
