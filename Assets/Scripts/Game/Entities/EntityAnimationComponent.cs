897dd24ee6cfcd45ef10eaeb435e5bf88869ad21
craftwill <william-gin1@hotmail.com>
2024-02-28 14:39:03 -0500

    Basic Animations added

--- Assets/Scripts/Game/Entities/EntityAnimationComponent.cs
+++ Assets/Scripts/Game/Entities/EntityAnimationComponent.cs
@@ -20,6 +20,8 @@ namespace Kraken
             if (_animStateMachine.GetAnimator() == null) return;
 
             _animStateMachine.GetAnimator().speed = Random.Range(0.9f, 1.1f);
+
+            SetLoopedStateIdle();
         }
 
         public void PlayHurtAnim()
@@ -34,12 +36,30 @@ namespace Kraken
             }
         }
 
+        public void SetLoopedStateIdle()
+        {
+            _animStateMachine.SetLoopedState(EntityAnimState.Idle, _animatorPrefix, _animatorSuffix);
+        }
+
+        public void SetLoopedStateWalking()
+        {
+            _animStateMachine.SetLoopedState(EntityAnimState.Walk, _animatorPrefix, _animatorSuffix);
+        }
+
+        public void PlayBasicAttackAnimation() 
+        {
+            _animStateMachine.PlayAnimOnce(EntityAnimState.BasicAttack, _animatorPrefix, _animatorSuffix);
+        }
+
         // Animations used by every entity
         public class EntityAnimState : KrakenBaseAnimState
         {
             static public readonly EntityAnimState Idle = new EntityAnimState("Idle", 1f);
             static public readonly EntityAnimState Walk = new EntityAnimState("Walk", 1f);
             static public readonly EntityAnimState Die = new EntityAnimState("Die", 1f);
+
+            // Player's will use a different animState because they use combo attacks.
+            static public readonly EntityAnimState BasicAttack = new EntityAnimState("BasicAttack", 1f);
 
             public EntityAnimState(string pClipName, float pAnimSpeed = 1f, int pNbVariations = -1)
                 : base(pClipName, pAnimSpeed, pNbVariations)
