c852374cb8813bdc7821b9d3098df545a3f87221
_Dante09 <mathglmtl@gmail.com>
2024-02-29 22:02:17 -0500

    revert

--- Assets/Scripts/Data/Enemies/EnemyConfigSO.cs
+++ Assets/Scripts/Data/Enemies/EnemyConfigSO.cs
@@ -13,8 +13,7 @@ namespace Kraken
         public float maxHealth = 3f;
         public float moveSpeed = 1.0f;
 
-        [Tooltip("Basic combat stats for melee and range minions"), Header("Basic Combat")]
-        public bool isRanged = false;
+        [Header("Combat")]
         public float attackRange = 2.5f;
         public float damageDealt = 1;
         public float attackCooldown = 1f;
