ca2e5815f17a8f5dbc38e062e9b992960aba6b6d
Leah Fortin <skaryu64@gmail.com>
2024-03-03 13:11:27 -0500

    fix highlight for multiplayer

---
+++ Assets/Scripts/Game/Objectives/MinimapHighlightComponent.cs
@@ -0,0 +1,24 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kraken
+{
+    public class MinimapHighlightComponent : MonoBehaviourPun
+    {
+        [SerializeField] GameObject _border;
+        public void SetVisible(bool visible)
+        {
+            if (!PhotonNetwork.IsMasterClient) return;
+            photonView.RPC(nameof(RPC_All_SetHighlightVisible), RpcTarget.All, visible);
+        }
+
+        [PunRPC]
+        private void RPC_All_SetHighlightVisible(bool visible)
+        {
+            _border.SetActive(visible);
+        }
+    }
+
+}
