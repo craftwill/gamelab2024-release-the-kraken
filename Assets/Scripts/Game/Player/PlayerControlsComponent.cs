ccd2a7b50c8a0755215892ab91422c603286a035
Leah Fortin <skaryu64@gmail.com>
2024-03-25 15:48:00 -0400

    test

--- Assets/Scripts/Game/Player/PlayerControlsComponent.cs
+++ Assets/Scripts/Game/Player/PlayerControlsComponent.cs
@@ -88,6 +88,10 @@ namespace Kraken
                 EventManager.AddEventListener(EventNames.UpdateCameraSettings, HandleCameraSettingsChanged);
                 EventManager.AddEventListener(EventNames.TogglePause, OnTogglePause);
             }
+            else
+            {
+                _controller.gameObject.SetActive(false);
+            }
         }
 
         private void OnDestroy()
