eef984b5422255989a554d8aef1772c56ea96ec8
Leah Fortin <skaryu64@gmail.com>
2024-03-24 20:31:13 -0400

    only master can move to next night and only once

--- Assets/Scripts/UI/VictoryDefeat/VictoryScreenUI.cs
+++ Assets/Scripts/UI/VictoryDefeat/VictoryScreenUI.cs
@@ -2,16 +2,19 @@
 using Bytes;
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Kraken.UI
 {
     public class VictoryScreenUI : BaseEndGameScrenUI
     {
         [SerializeField] private GameFlowManager _gameFlowManager;
+        [SerializeField] private Button _btnNextNight;
         private void Start()
         {
             _gameFlowManager = Object.FindObjectOfType<GameFlowManager>(); //temp but i don't wanna lock the game scene
             EventManager.AddEventListener(EventNames.ShowVictoryScreenUI, HandleShowScreenUI);
+            _btnNextNight.interactable = PhotonNetwork.IsMasterClient;
         }
 
         private void OnDestroy()
@@ -21,6 +24,7 @@ namespace Kraken.UI
 
         public void Btn_NextNight()
         {
+            _btnNextNight.interactable = false;
             _gameFlowManager.GoToNextNight();
         }
 
