e32c808fa54ffc37db557dfa0c3a3b4b0a6c6e1a
Leah Fortin <skaryu64@gmail.com>
2024-04-01 12:02:05 -0400

    minimap icons

---
+++ Assets/Scripts/UI/HUD/MinimapAlwaysFaceNorthComponent.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kraken
+{
+    public class MinimapAlwaysFaceNorthComponent : MonoBehaviour
+    {
+        [SerializeField] Vector3 direction = Vector3.down;
+        private void Update()
+        {
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
+    }
+
+}
