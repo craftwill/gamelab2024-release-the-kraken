59089689826895a2ce266a525b11bbe46094e5ca
Leah Fortin <skaryu64@gmail.com>
2024-04-01 14:23:28 -0400

    end objective

--- Assets/Scripts/Data/Objectives/ObjectiveSO.cs
+++ Assets/Scripts/Data/Objectives/ObjectiveSO.cs
@@ -12,6 +12,10 @@ namespace Kraken
         [Tooltip("Time before next objective spawns")] public int objectiveTimer = 60;
 
         public virtual void TriggerObjective(ObjectiveInstance instance) { }
-        public virtual void EndObjective(ObjectiveInstance instance) { }
+        public virtual void EndObjective(ObjectiveInstance instance) 
+        {
+            instance.Zones?.ForEach(x => x?.SetIsActiveZone(false));
+            EventManager.Dispatch(EventNames.UpdateCurrentZoneOccupancyUI, new UpdateZoneOccupancyUIData(0, 10));
+        }
     }
 }
